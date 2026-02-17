0c5676664a3ffccc349a933d839814d8c59d804f
Sarah Hazell Pickering <sarahhp@medisin.uio.no>
2026-02-17 03:05:22 +0100

    Adding variability estimates to Monocle trajectories in figure6

--- code/plot_g2g_results_on_tsses.py
+++ code/plot_g2g_results_on_tsses.py
@@ -6,6 +6,7 @@ import seaborn as sb
 import pandas as pd
 import warnings
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 warnings.filterwarnings("ignore")
 
 from genes2genes import Main
@@ -24,8 +25,9 @@ load_data =True
 
 bvw = ["#1F78B4","#FF7F00"]
 clusters = dict(zip([str(i) for i in range(12)],
-            ["#5b859e", "#1e395f" ,"#75884b", "#1e5a46", "#df8d71", "#af4f2f" ,
-            "#d48f90", "#732f30", "#ab84a5", "#59385c", "#d8b847", "#b38711"]))
+            ["#417BA1", "#ff1493", "#ffff2e", "#345634" ,"#8b0000",
+            "#ff6700", "#93C0D5" ,"#8b4513", "#9248A7", "#1c8859",
+            "#474747", "#8fbc8f", "#d2b48c"]))
 
 ## Open previous alignment results
 os.makedirs(fig_path, exist_ok=True)
@@ -34,19 +36,21 @@ with open(odir + '/g2g_aligner.pkl', 'rb') as file:
     
 ## Plot a TSS
 genes=[
- "P:r2@BRD4", "P:r1@PROX1", "P:r1@ENPP5",
- "P:r3@EPB41L3", "P:r1@PPP1R1A", "P:r2@GREB1L",
- "P:r1@CLDN7", "P:r1@PCSK2", "P:r1@KCNB1",
- "P:r1@TSPAN19", "P:r3@PEMT", "P:r1@PEMT",
- "P:r1@PLIN1", "P:r1@ICAM1", "P:r1@PM20D1",
- "P:r1@MGP", "P:r1@FABP4", "P:r1@FABP3",
- "P:r1@LPL", "P:r1@PCK1", "P:r1@ACTA2",
- "P:r1@TAGLN", "P:r1@PHLDA1", "P:r3@CD36",
- "P:r6@CD36", "P:r1@CD36", "P:r2@CD36",
- "P:r1@CXCL8", "P:r1@ITPKA", "P:r1@FPR2",
- "P:r1@AGXT", "P:r1@CA4", "P:r1@MMP7",
- "P:r1@HIST1H2BI", "P:r1@DIO2"
+# "P:r2@BRD4", "P:r1@PROX1", "P:r1@ENPP5",
+ #"P:r3@EPB41L3", "P:r1@PPP1R1A", "P:r2@GREB1L",
+ #"P:r1@CLDN7", "P:r1@PCSK2", "P:r1@KCNB1",
+ #"P:r1@TSPAN19", "P:r3@PEMT", "P:r1@PEMT",
+ "P:r1@PLIN1", #"P:r1@ICAM1", "P:r1@PM20D1",
+ "P:r1@MGP", #"P:r1@FABP4", "P:r1@FABP3",
+ "P:r1@LPL", #"P:r1@PCK1", "P:r1@ACTA2",
+ "P:r1@TAGLN", "P:r1@PHLDA1",# "P:r3@CD36",
+ #"P:r6@CD36", "P:r1@CD36", "P:r2@CD36",
+ #"P:r1@CXCL8", "P:r1@ITPKA", "P:r1@FPR2",
+ #"P:r1@AGXT", "P:r1@CA4", "P:r1@MMP7",
+ #"P:r1@HIST1H2BI", "P:r1@DIO2"
 ]
+
+
 
 for gene in genes:
     if gene not in aligner.results_map.keys():
@@ -54,6 +58,31 @@ for gene in genes:
     else:
         print("Plotting " + gene)
         VisualUtils.plotTimeSeries(gene, aligner, plot_cells=True)
+        
+        #Keep only left-most figure (trajectory)
+        axes = plt.gcf().get_axes()
+        axes[1].remove()
+        axes[2].remove()
+        
+        #Re-add x axis
+        left_ax = axes[0]
+        left_ax.set_axis_on()
+        left_ax.set_xlabel("Pseudotime (Monocle)")
+        left_ax.set_ylabel("TSS expression")
+        
+        #change colours
+        colors = ["#FF7F00", "#1F78B4"]
+        
+        for line, col in zip(left_ax.lines, colors):
+            line.set_color(col)
+        
+        scatters = [c for c in left_ax.collections if hasattr(c, "get_offsets")]
+        for coll, col in zip(scatters, colors):
+            alpha = coll.get_alpha() or 1.0 #keep previous alpha
+            coll.set_facecolor(mcolors.to_rgba(col, alpha))
+
+        with open("{}/{}_alignment_fig.pkl".format(fig_path, gene), 'wb') as file:
+            pickle.dump(plt.gcf(), file)
         plt.savefig("{}/{}_alignment.png".format(fig_path, gene), format="png", dpi=300, bbox_inches="tight")
         plt.close() 
 
